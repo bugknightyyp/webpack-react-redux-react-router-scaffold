6b0527da542d69dbde7e89f22b39e92655690b88
yangyongpeng <yongpeng.yang@xfxb.net>
2018-01-06 18:48:30 +0800

    init project

---
+++ config/webpack.vendor.config.js
@@ -0,0 +1,31 @@
+
+
+/**
+ * 
+ * 
+ *  http://blog.csdn.net/mjzhang1993/article/details/64440148
+ *  https://github.com/zzlw/webpack-dev-super上海
+ *
+ */
+const path = require('path');
+const webpack = require('webpack')
+
+
+
+module.exports = {
+	context: __dirname,
+	entry: {
+        vendor: ['react', 'react-dom', 'redux', 'react-redux', 'react-router-dom'],
+    },
+	output: {
+		filename: "vendor.js", // best use [hash] here too
+		path: path.resolve(__dirname, "dll"),
+		library: "vendor_lib_[hash]",
+	},
+	plugins: [
+		new webpack.DllPlugin({
+			name: "[name]_[hash]",
+			path: path.resolve(__dirname, "dll/vendor-manifest.json"),
+		}),
+	],
+};
